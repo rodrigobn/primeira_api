389c98b8a9a506c7251f9ed1911f8a94ca3de51a
Rodrigo Brito do Nascimento <rodrito90@gmail.com>
2021-02-22 12:04:03 -0300

    Refactored code

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -1,27 +1,21 @@
-/**
- * O describe é uma global do Mocha usada para descrever suítes de testes que contém um ou mais 
- * casos de testes e/ou contém outras suítes de testes.
- */
-describe('Routes: Products', () => {
+
+const request = require('supertest');
+const app = request('../src/app');
+const req = request(app);
+
+describe('Integration test exaple', function() {
     const defaultProduct = {
         name: 'Default product',
         description: 'product description',
         price: 100
     }
-
-    /**
-     * primeira suíte de testes
-     */
-    describe('GET /products', () => {
-        //it = descrever um caso de teste.
-        it('should return a list of products', done => { //done = informar ao Mocha quando o teste finalizou
-
-            request
-                .get('/products')
-                .end((err, res) => {
-                    expect(res.body[0]).to.eql(defaultProduct);
-                    done(err);
-                })
+    
+    it('get /products', function(done) {
+        req
+        .get('/products')
+        .end((err, res) => {
+            expect(res.body[0]).to.eql(defaultProduct);
+            done(err);
         })
-    })
-})
+    });
+});
